72de197d36ba2bf5baf75189acad482a9cc4f18c
Andrei Razborov <andreyrd94@gmail.com>
2025-05-21 19:46:37 +0300

    ksp

--- app/src/main/java/com/ar11/mobilecryptowallet/util/CryptoConverter.kt
+++ app/src/main/java/com/ar11/mobilecryptowallet/util/CryptoConverter.kt
@@ -3,23 +3,24 @@ package com.ar11.mobilecryptowallet.util
 import androidx.room.TypeConverter
 import com.ar11.mobilecryptowallet.dto.CryptosModel
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class CryptoConverter {
     @TypeConverter
-    fun listToJsonString(value: List<CryptosModel>?): String {
+    fun listToJsonString(value: MutableList<CryptosModel>?): String {
         if(value.isNullOrEmpty()) return ""
         return Gson().toJson(value)
     }
 
     @TypeConverter
-    fun jsonStringToList(value: String): List<CryptosModel> {
+    fun jsonStringToList(value: String): MutableList<CryptosModel> {
+        try {
+            val cryptosModelListType = object : TypeToken<MutableList<CryptosModel>?>() {}.type
 
-        return try {
-            val list = Gson().fromJson(value, Array<CryptosModel>::class.java).toList()
-            list
+            return Gson().fromJson(value, cryptosModelListType)
         } catch (e: Exception) {
-            emptyList()
+            return mutableListOf()
         }
-
     }
 }
+
